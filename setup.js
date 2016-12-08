dbf565cf8e9516122ebc26747f71bad07652bb71
wishduenas <wishi0jl@gmail.com>
2016-12-07 21:40:33 -0600

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,17 +1,18 @@
 var datum = new Date();
-var colour = ["#00CED1","#FFA500","#32CD32","#FF4500","#C71585","#FF7F50","#FFD700"];
-document.getElementById("loader").style.backgroundColor = colour[datum.getDay()];
+var colour = [["#00CED1","#50FEF1"],["#FFA500","#FFF550"],["#32CD32","82FD82"],["#FF4500","#FF9550"],["#C71585","F765D5"],["#FF7F50","#FFCFA0"],["#FFD700","#FFF750"]];
+document.getElementById("loader").style.backgroundColor = colour[datum.getDay()][0];
 var tag = ["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"];
 var monat = ["JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER"];
 document.getElementById("date").innerHTML = tag[datum.getDay()]+" "+datum.getDate()+". "+monat[datum.getMonth()]+" "+datum.getFullYear();
-document.getElementById("square").style.backgroundColor = colour[datum.getDay()];
-document.getElementsByClassName("rect")[0].style.backgroundColor = colour[datum.getDay()];
-document.getElementsByClassName("rect")[1].style.backgroundColor = colour[datum.getDay()];
+document.getElementById("square").style.backgroundColor = colour[datum.getDay()][0];
+document.getElementsByClassName("rect")[0].style.backgroundColor = colour[datum.getDay()][0];
+document.getElementsByClassName("rect")[1].style.backgroundColor = colour[datum.getDay()][0];
 var rph = document.getElementsByClassName("rectph");
 for (i = 0; i < rph.length; i++) {
-   rph[i].style.backgroundColor = colour[datum.getDay()];
+   rph[i].style.backgroundColor = colour[datum.getDay()][0];
 }
-var colr = "a:hover {color:"+colour[datum.getDay()]+"}";
+document.getElementById("bio").style.backgroundColor = colour[datum.getDay()][1];
+var colr = "a:hover {color:"+colour[datum.getDay()][0]+"}";
 document.styleSheets[0].insertRule(colr,0);
 var hei = document.getElementsByClassName("content")[0].clientHeight;
 var divdr = document.getElementsByClassName("divider");
